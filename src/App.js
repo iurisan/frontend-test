95ad9c089edca87bf9f0de4208df9414184edd49
iurisantanna <iuri.santanna@tourradar.com>
2024-07-23 12:36:02 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App = () => {
+
+  return (
+    <>
+      <header>
+        <img src="./images/dhg_whole.png" />
+      </header>
+      <main>
+        Login page
+      </main>
+    </>
+  );
+}
+
+export default App;
